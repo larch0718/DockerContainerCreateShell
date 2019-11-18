91a697d19bb3f261eb01ca69e52f28db0dfc8828
落叶松 <zhangsonglch@163.com>
2019-11-18 12:10:25 +0800

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum remove docker*
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum makecache fast
+sudo yum -y install docker-ce
+sudo usermod -aG docker $(whoami)
+exit 0
